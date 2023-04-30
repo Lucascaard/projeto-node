16f01a2494bc10be95d0d47f932dbc8f0e03171f
Lucas Cardoso <adslucascardoso@gmail.com>
2023-04-30 18:13:22 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 require("./config/dbConfig");
 
-app.use(cors({ origin: 'http://127.0.0.1:5501' }));
+app.use(cors({ origin: 'http://127.0.0.1:5500' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
