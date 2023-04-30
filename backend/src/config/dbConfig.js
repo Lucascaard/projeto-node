8149b686939208f1b90b6be693e134441897a007
Lucas Cardoso <adslucascardoso@gmail.com>
2023-04-29 23:43:18 -0300

    Update dbConfig.js

--- backend/src/config/dbConfig.js
+++ backend/src/config/dbConfig.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 const dbConfig = "mongodb://0.0.0.0:27017/cliente";
-//const dbConfig = "mongodb+srv://adslucascardoso:conection2500@cluster0.gsktlgs.mongodb.net/pessoa?retryWrites=true&w=majority";
-
 (async () => {
     try {
         await mongoose.connect(dbConfig, {
