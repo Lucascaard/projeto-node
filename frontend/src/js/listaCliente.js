e5548e0d617579a67f26e647a4a8551ea47f57e3
Lucas Cardoso de Carvalho <adslucascardoso@gmail.com>
2023-05-06 23:07:24 -0300

    update

---
+++ frontend/src/js/listaCliente.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const section = document.querySelector("section");
+  
+    fetch("http://localhost:8082/resumo")
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(cliente => {
+  
+        const div = document.createElement("div");
+        const nome = document.createElement("h2");
+        const cpf = document.createElement("h2");
+        const idCliente = document.createElement("h2");
+  
+        nome.textContent = `Nome: ${cliente.nome}`;
+        cpf.textContent = `CPF: ${cliente.cpf}`;
+        idCliente.textContent = `ID: ${cliente.idCliente}`;
+        
+        div.appendChild(nome);
+        div.appendChild(cpf);
+        div.appendChild(idCliente);
+  
+        document.createElement("hr");
+        div.appendChild(document.createElement("hr"));
+        section.appendChild(div);
+        
+        
+      });
+    })
+    .catch(error => console.log(error));
+  });
+  
+  
+  
+  
+  
